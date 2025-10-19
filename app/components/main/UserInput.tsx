31e6c02ab157bfa4047b2378c3eb5de3049dc624
JesusItDev <jesus.it.dev@gmail.com>
2025-10-19 14:45:50 -0400

    Modified onSubmit funtioc from userInput component to be able to handle Streams

--- app/components/main/UserInput.tsx
+++ app/components/main/UserInput.tsx
@@ -2,7 +2,11 @@
 import { useState } from "react";
 import { useContext } from "react";
 import { MessageContext } from "../../context/MessageContext";
-import { MessageContextType } from "../../utils/interfaces";
+import { IncomingMessageContext } from "../../context/IncomingMessageContext";
+import {
+  MessageContextType,
+  IncomingMessageContextType,
+} from "../../utils/interfaces";
 
 const UserInput = () => {
   const [prompt, setPrompt] = useState<string>("");
@@ -10,12 +14,21 @@ const UserInput = () => {
 
   const messageContext = useContext<MessageContextType | null>(MessageContext);
 
+  const incomingMessageContext = useContext<IncomingMessageContextType | null>(
+    IncomingMessageContext
+  );
+
   //Check if context is provided
   if (!messageContext) {
     return <div>Context not provided</div>;
   }
 
+  if (!incomingMessageContext) {
+    return <div>Context not provided</div>;
+  }
+
   const { setMessages } = messageContext;
+  const { setIncomingMessage } = incomingMessageContext || {};
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -33,14 +46,36 @@ const UserInput = () => {
       body: JSON.stringify({ prompt }),
     });
 
-    const result = await response.json();
+    if (!response.body) return;
 
-    setMessages((prevState) => [
-      ...prevState,
-      { role: "assistant", content: result },
-    ]);
+    const reader = response.body
+      .pipeThrough(new TextDecoderStream())
+      .getReader();
 
-    setIsLoading(false);
+    if (reader) setIsLoading(false);
+
+    let incomingMessage = "";
+
+    while (true) {
+      const { done, value } = await reader.read();
+
+      if (done) {
+        setMessages((prevState) => [
+          ...prevState,
+          { role: "assistant", content: incomingMessage },
+        ]);
+
+        setIncomingMessage("");
+
+        break;
+      }
+
+      if (value) {
+        incomingMessage += value;
+
+        setIncomingMessage(incomingMessage);
+      }
+    }
   };
 
   return (
