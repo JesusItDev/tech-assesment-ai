15d49ef81988eb736d8ed28dcecbbe59766742e8
JesusItDev <jesus.it.dev@gmail.com>
2025-10-19 14:42:03 -0400

    Created and added ChatMessageItem to conversation component and passed incoming message context

---
+++ app/components/main/ChatMessageItem.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ChatMessage } from "../../utils/interfaces";
+
+const ChatMessageItem = ({ message }: { message: ChatMessage }) => {
+  return <div>ChatMessageItem</div>;
+};
+
+export default ChatMessageItem;
